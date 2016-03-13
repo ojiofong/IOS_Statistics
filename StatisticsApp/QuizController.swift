2b9a1ad6bc29e195c7e2d7e22b4df16ff4d00add
Oji Ofong <ojiofong@gmail.com>
2016-03-13 01:02:18 -0500

    Added QuizController class

---
+++ StatisticsApp/QuizController.swift
@@ -0,0 +1,20 @@
+//
+//  QuizController.swift
+//  StatisticsApp
+//
+//  Created by Oji Ofong on 3/13/16.
+//  Copyright © 2016 Oji Ofong. All rights reserved.
+//
+
+import UIKit
+
+class QuizController : UIViewController{
+    
+    var textTitle = "Default text value"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = textTitle
+    }
+    
+}
